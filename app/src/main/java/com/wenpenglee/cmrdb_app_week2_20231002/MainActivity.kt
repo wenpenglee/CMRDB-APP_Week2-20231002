cf93d9d76a340a5ca1be9b9952a32bfe86f99af3
wenpenglee <leewenpengl@gmail.com>
2023-10-02 14:46:58 +0800

    Initial commit

---
+++ app/src/main/java/com/wenpenglee/cmrdb_app_week2_20231002/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wenpenglee.cmrdb_app_week2_20231002
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CMRDB-APP_Week2-20231002"
+include(":app")
